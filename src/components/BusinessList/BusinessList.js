c0292cec81c06a27ff3630a60b8ceffb770564f9
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-07-17 15:49:58 -0400

    Create a map() callback function to dynamically return a list of Business components.

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -2,16 +2,21 @@ import React from 'react';
 import './BusinessList.css';
 import Business from '../Business/Business';
 
+/* 
+The businesses array (in App.js) contains a list of businesses. 
+The businesses array is accessed through the businesses prop in the Business List component. 
+BusinessList will iterate (map) through this list to render a list of businesses. 
+*/
+
 class BusinessList extends React.Component {
     render() {
         return (
             <div className="BusinessList">
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
+                { 
+                    this.props.businesses.map(business => {
+                            return <Business business={business} />;
+                        }) 
+                }
             </div>
         );
     }
