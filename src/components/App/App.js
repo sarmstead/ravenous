cb2778684d7e0520f74fa1baabb3108bac5414ff
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-08-01 15:30:35 -0400

    Add Yelp functionality to SearchBar component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,33 +2,26 @@ import React from 'react';
 import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
+import Yelp from '../../util/Yelp';
 
 // The business object contains multiple key/value pairs that form the individual business cards on the home page of the Ravenous App.
-const business = {
-  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddignton Way',
-  city: 'Flavortown',
-  state: 'NC',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-};
+
 
 // The purpose of the businesses array is to simulate a list of businesses (multiple instances of the business object).
-const businesses = [
-  business,
-  business,
-  business,
-  business,
-  business,
-  business, 
-]
+
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      businesses: []
+    }
+    this.searchYelp = this.searchYelp.bind(this);
+  }
   searchYelp(term, location, sortBy) {
-    console.log(`Searching Yelp for ${term} and ${location}, sorted by ${sortBy}.`);
+    Yelp.search(term, location, sortBy).then(businesses => {
+      this.setState({ businesses: businesses } );
+    });
   }
 
   render() {
@@ -36,7 +29,7 @@ class App extends React.Component {
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar searchYelp={this.searchYelp} />
-        <BusinessList businesses={businesses} />
+        <BusinessList businesses={this.state.businesses} />
       </div>
     );
   }
