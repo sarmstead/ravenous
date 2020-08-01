94cac03fcd14eeb4fde3c3316cdc20cd7a5a2a39
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-08-01 17:54:12 -0400

    Add Google Maps link functionality

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Business.css';
 
+
 // The purpose of the <Business /> component is to represent how an individual business (a restaurant) in Ravenous will be formatted and styled
 class Business extends React.Component {
     render() {
@@ -12,7 +13,7 @@ class Business extends React.Component {
                 <h2>{this.props.business.name}</h2>
                 <div className="Business-information">
                     <div className="Business-address">
-                        <p>{this.props.business.address}</p>
+                        <a href={`https://www.google.com/maps/search/?api=1&query=${this.props.business.name.replace('+', ' ')}+${this.props.business.address.replace('+', ' ')}+${this.props.business.city.replace('+', ' ')}+${this.props.business.state}+${this.props.business.zipCode}`} target="_blank" rel="noopener noreferrer"><p>{this.props.business.address}</p></a>
                         <p>{this.props.business.city}</p>
                         <p>{`${this.props.business.state} ${this.props.business.zipCode}`}</p>
                     </div>
